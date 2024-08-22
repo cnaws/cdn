2d199903f930afe1e2d504500d3baee221a45e8b
Pomfox <128436378+cnaws@users.noreply.github.com>
2024-08-22 14:13:05 +0800

    Update master.sh

--- master.sh
+++ master.sh
@@ -2,7 +2,7 @@
 
 set -o errexit
 
-#判断系统版本
+# 判断系统版本
 check_sys(){
     local checkType=$1
     local value=$2
@@ -12,42 +12,34 @@ check_sys(){
     local packageSupport=''
 
     if [[ "$release" == "" ]] || [[ "$systemPackage" == "" ]] || [[ "$packageSupport" == "" ]];then
-
         if [[ -f /etc/redhat-release ]];then
             release="centos"
             systemPackage="yum"
             packageSupport=true
-
         elif cat /etc/issue | grep -q -E -i "debian";then
             release="debian"
             systemPackage="apt"
             packageSupport=true
-
         elif cat /etc/issue | grep -q -E -i "ubuntu";then
             release="ubuntu"
             systemPackage="apt"
             packageSupport=true
-
         elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat";then
             release="centos"
             systemPackage="yum"
             packageSupport=true
-
         elif cat /proc/version | grep -q -E -i "debian";then
             release="debian"
             systemPackage="apt"
             packageSupport=true
-
         elif cat /proc/version | grep -q -E -i "ubuntu";then
             release="ubuntu"
             systemPackage="apt"
             packageSupport=true
-
         elif cat /proc/version | grep -q -E -i "centos|red hat|redhat";then
             release="centos"
             systemPackage="yum"
             packageSupport=true
-
         else
             release="other"
             systemPackage="other"
@@ -91,7 +83,7 @@ install_depend() {
 }
 
 get_sys_ver() {
-cat > /tmp/sys_ver.py <<EOF
+    cat > /tmp/sys_ver.py <<EOF
 import platform
 import re
 
@@ -101,38 +93,30 @@ if sys_ver.startswith("centos-7"):
     sys_ver = "centos-7"
 if sys_ver.startswith("centos-6"):
     sys_ver = "centos-6"
-print sys_ver
+print(sys_ver)
 EOF
-echo `python /tmp/sys_ver.py`
+    echo `python /tmp/sys_ver.py`
 }
 
 download(){
-  local url1=$1
-  local url2=$2
-  local filename=$3
+    local url1=$1
+    local url2=$2
+    local filename=$3
 
-  # 检查文件是否存在
-  # if [[ -f $filename ]]; then
-  #   echo "$filename 文件已经存在，忽略"
-  #   return
-  # fi
-
-  speed1=`curl -m 5 -L -s -w '%{speed_download}' "$url1" -o /dev/null || true`
-  speed1=${speed1%%.*}
-  speed2=`curl -m 5 -L -s -w '%{speed_download}' "$url2" -o /dev/null || true`
-  speed2=${speed2%%.*}
-  echo "speed1:"$speed1
-  echo "speed2:"$speed2
-  url="$url1\n$url2"
-  if [[ $speed2 -gt $speed1 ]]; then
-    url="$url2\n$url1"
-  fi
-  echo -e $url | while read l;do
-    echo "using url:"$l
-    wget --dns-timeout=5 --connect-timeout=5 --read-timeout=10 --tries=2 "$l" -O $filename && break
-  done
-  
-
+    speed1=`curl -m 5 -L -s -w '%{speed_download}' "$url1" -o /dev/null || true`
+    speed1=${speed1%%.*}
+    speed2=`curl -m 5 -L -s -w '%{speed_download}' "$url2" -o /dev/null || true`
+    speed2=${speed2%%.*}
+    echo "speed1:"$speed1
+    echo "speed2:"$speed2
+    url="$url1\n$url2"
+    if [[ $speed2 -gt $speed1 ]]; then
+        url="$url2\n$url1"
+    fi
+    echo -e $url | while read l;do
+        echo "using url:"$l
+        wget --dns-timeout=5 --connect-timeout=5 --read-timeout=10 --tries=2 "$l" -O $filename && break
+    done
 }
 
 sync_time(){
@@ -158,25 +142,14 @@ sync_time(){
     if /sbin/hwclock -w;then
         return
     fi 
-
-
 }
 
-need_sys() {
-    SYS_VER=`python -c "import platform;import re;sys_ver = platform.platform();sys_ver = re.sub(r'.*-with-(.*)-.*','\g<1>',sys_ver);print sys_ver;"`
-    if [[ $SYS_VER =~ "Ubuntu-16.04" ]];then
-      echo "$sys_ver"
-    elif [[ $SYS_VER =~ "centos-7" ]]; then
-      SYS_VER="centos-7"
-      echo $SYS_VER
-    else  
-      echo "目前只支持ubuntu-16.04和Centos-7"
-      exit 1
-    fi
-}
+# 直接指定版本
+VER="v5.6.13"
+tar_gz_name="cdnfly-agent-${VER}-centos-7.tar.gz"
 
 install_depend
-need_sys
+get_sys_ver
 sync_time
 
 # 解析命令行参数
@@ -193,44 +166,20 @@ while true ; do
     esac
 done
 
-if [[ $VER == "" ]]; then
-  # 获取最新版本
-  echo "获取最新版..."
-  latest_version=`curl -s 'http://auth.fikkey.com/master/upgrades?latest=1' | grep -Po 'v\d+\.\d+.\d+' || true`
-  if [[ "$latest_version" == "" ]]; then
-    echo "获取最新版失败，请先登录官网填入主控IP"
-    exit 1
-  fi
-
-  echo "最新版本为$latest_version"
-  dir_name="cdnfly-master-$latest_version"
-  tar_gz_name="$dir_name-$(get_sys_ver).tar.gz"
-
-else
-  # 安装指定版本
-  if [[ ! `echo "$VER" | grep -P "^v\d+\.\d+\.\d+$"` ]]; then
-    echo "指定的版本格式不正确，应该类似为v4.0.1"
-    exit 1
-  fi
-
-  dir_name="cdnfly-master-$VER"
-  tar_gz_name="$dir_name-$(get_sys_ver).tar.gz"
-  echo "安装指定版本$VER"
-fi
-
 cd /opt/
 download "https://github.com/cnaws/cdn/raw/main/$tar_gz_name" "https://github.com/cnaws/cdn/raw/main/master/$tar_gz_name" "$tar_gz_name"
 
 tar xf $tar_gz_name
 rm -rf cdnfly
-mv $dir_name cdnfly
+mv cdnfly-agent-$VER cdnfly
 
 # 开始安装
-cd /opt/cdnfly/master
+cd /opt/cdnfly
 chmod +x install.sh
 ./install.sh $@
 
-if [ -f /opt/cdnfly/master/view/upgrade.so ]; then
-	sed -i "s/https:\/\/update.cdnfly.cn\//http:\/\/auth.cdnfly.cn\/\/\/\//g" /opt/cdnfly/master/view/upgrade.so
-	supervisorctl -c /opt/cdnfly/master/conf/supervisord.conf reload
+if [ -f /opt/cdnfly/view/upgrade.so ]; then
+    sed -i "s/https:\/\/update.cdnfly.cn\//http:\/\/auth.cdnfly.cn\/\/\/\//g" /opt/cdnfly/view/upgrade.so
+    supervisorctl -c /opt/cdnfly/conf/supervisord.conf reload
 fi
+
